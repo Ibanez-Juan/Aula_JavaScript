ae0ffcb6d6137e2e99cf13a44e85f7b4a601b904
Juan Ibanez <18juan.ibanez18@gmail.com>
2020-10-06 11:41:24 -0300

    Array and dictionary

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-var nome = "Juan Ibanez";
+var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/* var fruta = { nome: "maçã", cor: "vermelha" }
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+/* var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//console.log(lista);
+//lista.pop();
+console.log(lista[0]);
+console.log(lista.toString());
+console.log(lista.join(" | ")); */
+
+
+/* var nome = "Juan Ibanez";
 var n1 = 5;
 var n2 = 3;
 var frase = "Japão é o melhor time do mundo"
@@ -8,4 +25,4 @@ var frase = "Japão é o melhor time do mundo"
 console.log(nome);
 console.log(n1 * n2);
 console.log(frase.toLowerCase());
-//alert(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil")); */
